ee543487f251d32685b0eb25f808100b1f066e04
Lopuf <maxcalio28@gmail.com>
2021-05-17 22:07:45 +0200

    on peux selectionner, ca fé méssage rigolo

--- src/graphics/shapes/ui/ShapesController.java
+++ src/graphics/shapes/ui/ShapesController.java
@@ -1,16 +1,60 @@
 package graphics.shapes.ui;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
+import java.util.Iterator;
 
 import graphics.ui.Controller;
+import graphics.shapes.SCollection;
+import graphics.shapes.Shape;
+import graphics.shapes.attributes.SelectionAttributes;
 
 public class ShapesController extends Controller{
+	private SCollection model;
 	
 	public ShapesController(Object model) {
 		super(model);
+		this.model = (SCollection) model;
 		}
-
 	
-	public void mouseClicker(MouseEvent e) {
-		System.out.println("mouse click");
+	public Shape getElementClicked(Point p) {
+		Iterator<Shape> iterator = this.model.iterator();
+        while (iterator.hasNext()) {
+            Shape s = iterator.next();
+            if (s.getBounds().contains(p)) {
+                return s;
+            }
+        }
+        return null;
+	}
+	public void unselectOthers(Shape selected){
+		Iterator<Shape> iterator = this.model.iterator();
+        while (iterator.hasNext()) {
+            Shape s = iterator.next();
+            SelectionAttributes selectAtt = (SelectionAttributes)s.getAttributes("SELECTION");
+            if (selectAtt != null) {
+	            if (s == selected) {
+	                selectAtt.select();
+	            } else {
+	            	selectAtt.unselect();
+	            }
+	            System.out.println("\nEst ce que la prochaine figure est selectionnée?");
+	            System.out.println(s);
+	            System.out.println(selectAtt.isSelected());
+	            System.out.println("voila la reponse :D\n");
+            } else {
+            	System.out.println("SelectionAttributes non defini?");
+            }
+        }
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		Point mousePosition = e.getPoint();
+		Shape clickedShape = getElementClicked(mousePosition);
+		unselectOthers(clickedShape);
+		if(clickedShape==null) {
+			System.out.println("ya r ici bro");
+		} else {
+			System.out.println(clickedShape);
+		}
 	}
 }
--- src/graphics/ui/Controller.java
+++ src/graphics/ui/Controller.java
@@ -5,7 +5,6 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-
 import javax.swing.JPanel;
 
 public class Controller implements MouseListener, MouseMotionListener, KeyListener
